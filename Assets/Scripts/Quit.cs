b450022f3844eff1584ac9b12984cf48acc6484d
anshum404 <67569065+anshum404@users.noreply.github.com>
2023-05-25 15:55:41 +0530

    Scene Management Update

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void button_exit()
+    {
+    
+        Application.Quit();
+        Debug.Log("Quit");
+
+    }
+}
